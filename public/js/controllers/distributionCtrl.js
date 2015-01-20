974818695636c0a5319348216c4ef6c3fdaf9292
Kelvin Mbwilo <kelvinmbwilo@gmail.com>
2015-01-20 21:20:11 +0300

    fixing long loading of summary page

--- public/js/controllers/distributionCtrl.js
+++ public/js/controllers/distributionCtrl.js
@@ -7,21 +7,32 @@ angular.module("malariaApp")
 
         angular.forEach($scope.data.regions,function(value,index){
             var region = value;
-            $http.get("index.php/districts/region/"+value.id).success(function(distr){
-                region.districts = distr;
-                angular.forEach(region.districts,function(val){
-                    var district = val;
-                    $http.get("index.php/people/district/"+val.id).success(function(ppl){
-                        district.people = ppl;
-                    });
-                });
-            });
+////            $http.get("index.php/districts/region/"+value.id).success(function(distr){
+////                region.districts = distr;
+////                angular.forEach(region.districts,function(val){
+////                    var district = val;
+////                    $http.get("index.php/people/district/"+val.id).success(function(ppl){
+////                        district.people = ppl;
+////                    });
+////                });
+////            });
             $http.get("index.php/people/region/"+value.id).success(function(ppl){
                 region.people = ppl;
             });
         })
         $scope.showDistrictDetails = function(region){
             console.log("creating districts of "+ region.region);
+            if(region.districts == null){
+                $http.get("index.php/districts/region/"+region.id).success(function(distr){
+                    region.districts = distr;
+                    angular.forEach(region.districts,function(val){
+                        var district = val;
+                        $http.get("index.php/people/district/"+val.id).success(function(ppl){
+                            district.people = ppl;
+                        });
+                    });
+                });
+            }
             $scope.districtVisible[region.id] = true;
         }
 
@@ -29,6 +40,8 @@ angular.module("malariaApp")
             console.log("hiding districts of "+ region.region);
             $scope.districtVisible[region.id] = false;
         }
+
+
 
 
     });
